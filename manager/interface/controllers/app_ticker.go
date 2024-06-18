b0a69eb52b304306b6dcbf3b45ef411c62018f78
nakagahi <iwbb902@gmail.com>
2024-06-19 02:10:35 +0900

    tikcerの追加&テスト

---
+++ manager/interface/controllers/app_ticker.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "time"
+
+type AppTicker interface {
+	New(interval time.Duration, unit time.Duration) *time.Ticker
+}
+
+type appTicker struct{}
+
+func NewAppTicker() AppTicker {
+	return &appTicker{}
+}
+
+func (a *appTicker) New(interval time.Duration, unit time.Duration) *time.Ticker {
+	time.Sleep(time.Until(time.Now().Add(1 * unit).Truncate(unit)))
+	return time.NewTicker(interval)
+}
