084cd7dc330772df672527fba52750d996d16d4f
NaoyukiGouko <gouko.naoyuki@supership.jp>
2024-08-09 14:46:35 +0900

    [PB-688]models修正

--- manager/domain/models/creative.go
+++ manager/domain/models/creative.go
@@ -1,9 +1,8 @@
 package models
 
-import "strconv"
-
 type Creative struct {
 	ID               int      `db:"id" json:"id"`
+	DeliveryRate     int      `db:"delivery_rate" json:"delivery_rate"`
 	Link             *string  `db:"link" json:"link,omitempty"`
 	URL              string   `db:"url" json:"url"`
 	TTL              int64    `db:"ttl" json:"ttl"`
@@ -12,7 +11,7 @@ type Creative struct {
 	Type             string   `db:"type" json:"type"`
 	Extension        string   `db:"extension" json:"extension"`
 	Duration         *int     `db:"duration" json:"duration"`
-	SkipOffset       *int     `db:"skip_offset" json:"skip_offset"`
+	SkipOffset       int      `db:"skip_offset" json:"skip_offset"`
 	EndCardURL       *string  `db:"end_card_url" json:"end_card_url,omitempty"`
 	EndCardWidth     *float32 `db:"end_card_width" json:"end_card_width,omitempty"`
 	EndCardHeight    *float32 `db:"end_card_height" json:"end_card_height,omitempty"`
@@ -20,9 +19,10 @@ type Creative struct {
 	EndCardLink      *string  `db:"end_card_link" json:"end_card_link,omitempty"`
 }
 
-func (c *Creative) CreateDeliveryDataCreative() *DeliveryDataCreative {
+func (c *Creative) CreateDeliveryDataCreative(campaignID int) *DeliveryDataCreative {
 	return &DeliveryDataCreative{
-		CampaignID:       strconv.Itoa(c.ID),
+		CampaignID:       campaignID,
+		CreativeID:       c.ID,
 		Link:             c.Link,
 		URL:              c.URL,
 		TTL:              c.TTL,
@@ -31,11 +31,18 @@ func (c *Creative) CreateDeliveryDataCreative() *DeliveryDataCreative {
 		Type:             c.Type,
 		Extension:        c.Extension,
 		Duration:         c.Duration,
-		SkipOffset:       c.SkipOffset,
 		EndCardURL:       c.EndCardURL,
 		EndCardWidth:     c.EndCardWidth,
 		EndCardHeight:    c.EndCardHeight,
 		EndCardExtension: c.EndCardExtension,
 		EndCardLink:      c.EndCardLink,
+	}
+}
+
+func (c *Creative) CreateCampaignCreative() *CampaignCreative {
+	return &CampaignCreative{
+		ID:         c.ID,
+		Rate:       c.DeliveryRate,
+		SkipOffset: c.SkipOffset,
 	}
 }
